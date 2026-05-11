14176a26ca9b8dfae4d015df760cef10c49ad107
dias-andre <diaso.andre@outlook.com>
2026-05-11 18:04:31 -0300

    feat: add bean for GroupApplication

--- src/main/java/promo/bot/beedle_deals/infra/config/BeanConfiguration.java
+++ src/main/java/promo/bot/beedle_deals/infra/config/BeanConfiguration.java
@@ -2,14 +2,20 @@ package promo.bot.beedle_deals.infra.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import promo.bot.beedle_deals.core.application.DealApplication;
+import promo.bot.beedle_deals.application.DealApplication;
+import promo.bot.beedle_deals.application.GroupApplication;
+import promo.bot.beedle_deals.core.ports.GroupRepositoryPort;
 import promo.bot.beedle_deals.core.ports.NotificationServicePort;
-import promo.bot.beedle_deals.core.usecases.PostDealUseCase;
 
 @Configuration
 public class BeanConfiguration {
     @Bean
-    public DealApplication postDealUseCase(NotificationServicePort notificationPort) {
-        return new DealApplication(notificationPort);
+    public DealApplication postDealUseCase(NotificationServicePort notificationPort, GroupRepositoryPort repo) {
+        return new DealApplication(notificationPort, repo);
+    }
+
+    @Bean
+    public GroupApplication groupApplication(GroupRepositoryPort groupRepositoryPort) {
+        return new GroupApplication(groupRepositoryPort);
     }
 }
