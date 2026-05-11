782d1b7d7bb15a0d8531df39b52ce3cb7048c8db
dias-andre <diaso.andre@outlook.com>
2026-05-11 18:08:00 -0300

    feat: add custom runtime exception

---
+++ src/main/java/promo/bot/beedle_deals/core/exceptions/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package promo.bot.beedle_deals.core.exceptions;
+
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
