12eca2c1d6784f3a4e76a019993f2863fdb06592
dias-andre <diaso.andre@outlook.com>
2026-05-11 15:26:32 -0300

    feat: create exception for notification delivery errors

---
+++ src/main/java/promo/bot/beedle_deals/core/exceptions/NotificationDeliveryException.java
@@ -0,0 +1,7 @@
+package promo.bot.beedle_deals.core.exceptions;
+
+public class NotificationDeliveryException extends RuntimeException {
+    public NotificationDeliveryException(String message) {
+        super(message);
+    }
+}
