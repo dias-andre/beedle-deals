90d2ec451c991d992b63710e8a83158c8b0d73fa
dias-andre <diaso.andre@outlook.com>
2026-05-13 17:40:47 -0300

    refactor(api): improve route mapping

--- src/main/java/promo/bot/beedle_deals/adapters/in/web/NotificationController.java
+++ src/main/java/promo/bot/beedle_deals/adapters/in/web/NotificationController.java
@@ -4,17 +4,20 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import promo.bot.beedle_deals.core.usecases.PostDealUseCase;
 import promo.bot.beedle_deals.adapters.in.web.dtos.SendProductDTO;
 
 @RestController
+@RequestMapping("/api/v1/notifications")
 public class NotificationController {
     private final PostDealUseCase dealUseCase;
 
     public NotificationController(PostDealUseCase dealUseCase) {
         this.dealUseCase = dealUseCase;
     }
+
     @PostMapping
     public ResponseEntity<String> sendNotification(@Valid @RequestBody SendProductDTO dto) {
         this.dealUseCase.execute(dto.product().toDomain(), dto.externalGroupId());
