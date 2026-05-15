5cd7a955c687b23c86b675363424c99a4eba1f93
dias-andre <diaso.andre@outlook.com>
2026-05-15 19:07:52 -0300

    test(category): introduce new unit-tests

--- src/test/java/promo/bot/beedle_deals/adapters/out/db/GroupRepositoryAdapterTest.java
+++ src/test/java/promo/bot/beedle_deals/adapters/out/db/GroupRepositoryAdapterTest.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import promo.bot.beedle_deals.core.domain.Category;
 import promo.bot.beedle_deals.core.domain.Group;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -43,5 +45,31 @@ public class GroupRepositoryAdapterTest {
     void shouldGetEmptyGroupFromDb() {
         Optional<Group> gp = this.adapter.getGroupByExternalId("unknown");
         assertFalse(gp.isPresent());
+    }
+
+    @Test
+    void shouldCreateGroupWithCategory() {
+        Group gp = new Group("game-group", "1231231212", OffsetDateTime.now(), List.of(Category.GAMES));
+        this.adapter.saveGroup(gp);
+        List<Group> groupList = this.adapter.listGroupsByCategory(Category.GAMES);
+        assertEquals(gp.getExternalId(), groupList.get(0).getExternalId());
+    }
+
+    @Test
+    void shouldAddCategory() {
+        Group gp = new Group("hardware-group", "!23232", OffsetDateTime.now());
+        this.adapter.saveGroup(gp);
+        this.adapter.addGroupCategory(Category.HARDWARE, gp);
+        List<Group> grouplist = this.adapter.listGroupsByCategory(Category.HARDWARE);
+        assertEquals(gp.getExternalId(), grouplist.get(0).getExternalId());
+    }
+
+    @Test
+    void shouldRemoveCategory() {
+        Group gp = new Group("eletronic-group", "434343", OffsetDateTime.now(), List.of(Category.ELECTRONICS));
+        this.adapter.saveGroup(gp);
+        this.adapter.removeGroupCategory(Category.ELECTRONICS, gp);
+        List<Group> groupList = this.adapter.listGroupsByCategory(Category.ELECTRONICS);
+        assertEquals(0, groupList.size());
     }
 }
